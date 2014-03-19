ca988cbb6f5581c96b30d4635d14325cb913214f
Qinfan Wu <wuqinfan@gmail.com>
2014-03-19 10:54:32 -0400

    Add partial array visualization support

--- lib/AvlTypes.h
+++ lib/AvlTypes.h
@@ -2,15 +2,15 @@
 #define AVL_TYPES_H_
 
 #include <GL/freeglut.h>
+#include <initializer_list>
+#include <vector>
 #include <string>
 #include <iostream>
-using std::to_string;
-using std::ostream;
 
 class AvlObject
 {
 	public:
-		AvlObject(GLfloat x = 0, GLfloat y = 0, GLfloat width = 0, GLfloat height = 0,
+		AvlObject(GLfloat x = 0, GLfloat y = 0, GLfloat width = 9, GLfloat height = 15,
 				void *font = GLUT_BITMAP_9_BY_15)
 		{
 			_x = x;
@@ -22,17 +22,17 @@ class AvlObject
 
 		virtual ~AvlObject() {}
 
-		GLfloat x() const { return _x; }
-		GLfloat y() const { return _y; }
-		GLfloat width() const { return _width; }
-		GLfloat height() const { return _height; }
-		void *font() const { return _font; }
+		virtual GLfloat x() const { return _x; }
+		virtual GLfloat y() const { return _y; }
+		virtual GLfloat width() const { return _width; }
+		virtual GLfloat height() const { return _height; }
+		virtual void *font() const { return _font; }
 
-		void set_x(GLfloat x) { _x = x; }
-		void set_y(GLfloat y) { _y = y; }
-		void set_width(GLfloat width) { _width = width; }
-		void set_height(GLfloat height) { _height = height; }
-		void set_font(void *font) { _font = font; }
+		virtual void set_x(GLfloat x) { _x = x; }
+		virtual void set_y(GLfloat y) { _y = y; }
+		virtual void set_width(GLfloat width) { _width = width; }
+		virtual void set_height(GLfloat height) { _height = height; }
+		virtual void set_font(void *font) { _font = font; }
 
 		virtual void render() const = 0;
 
@@ -47,8 +47,8 @@ class AvlObject
 class AvlInt : public AvlObject
 {
 	public:
-		AvlInt(int v = 0, GLfloat x = 0, GLfloat y = 0, GLfloat width = 0,
-				GLfloat height = 0, void *font = GLUT_BITMAP_9_BY_15)
+		AvlInt(int v = 0, GLfloat x = 0, GLfloat y = 0, GLfloat width = 9,
+				GLfloat height = 15, void *font = GLUT_BITMAP_9_BY_15)
 			: AvlObject(x, y, width, height, font)
 		{
 			value = v;
@@ -56,11 +56,13 @@ class AvlInt : public AvlObject
 
 		virtual ~AvlInt() {}
 
-		friend ostream& operator<<(ostream &os, const AvlInt &v)
+		friend std::ostream& operator<<(std::ostream &os, const AvlInt &v)
 		{
 			os << v.value;
 			return os;
 		}
+
+		const AvlInt& operator=(int v) { value = v; return *this; }
 
 		const AvlInt& operator+() const { return *this; }
 		const AvlInt operator-() const
@@ -125,11 +127,84 @@ class AvlInt : public AvlObject
 		{
 			glColor4f(1.0, 0.0, 0.0, 0.0);
 			glRasterPos2f(x(), y());
-			glutBitmapString(font(), (const unsigned char *)(to_string(value).c_str()));
+			glutBitmapString(font(), (const unsigned char *)(std::to_string(value).c_str()));
 		}
 
 	private:
 		int value;
+};
+
+template <typename T>
+class AvlArray : public AvlObject, public std::vector<T>
+{
+	public:
+		AvlArray(size_t size) : std::vector<T>(size)
+		{
+			for (size_t i = 1; i < size; i++) {
+				(*this)[i].set_x((*this)[i-1].x() + (*this)[i-1].width() * 1.5);
+				(*this)[i].set_y((*this)[i-1].y());
+			}
+		}
+		AvlArray(const std::initializer_list<T> &l) : std::vector<T>(l) {
+			for (size_t i = 1; i < l.size(); i++) {
+				(*this)[i].set_x((*this)[i-1].x() + (*this)[i-1].width() * 1.5);
+				(*this)[i].set_y((*this)[i-1].y());
+			}
+		}
+
+		virtual ~AvlArray() {}
+
+		virtual GLfloat x() const { return (*this)[0].x(); }
+		virtual GLfloat y() const { return (*this)[0].y(); }
+		virtual GLfloat width() const
+		{
+			GLfloat w = 0;
+			for (auto& v: *this)
+				w += v.width();
+			return w;
+		}
+		virtual GLfloat height() const
+		{
+			GLfloat h = 0;
+			for (auto& v: *this)
+				h = v.height() > h ? v.height() : h;
+			return h;
+		}
+		virtual void *font() const { return (*this)[0].font(); }
+
+		virtual void set_x(GLfloat x)
+		{
+			(*this)[0].set_x(x);
+			for (size_t i = 1; i < this->size(); i++)
+				(*this)[i].set_x((*this)[i-1].x() + (*this)[i-1].width());
+		}
+		virtual void set_y(GLfloat y)
+		{
+			for (auto& v: *this)
+				v.set_y(y);
+		}
+		virtual void set_width(GLfloat width)
+		{
+			for (auto& v: *this)
+				v.set_width(width / this->size());
+			set_x((*this)[0].x());
+		}
+		virtual void set_height(GLfloat y)
+		{
+			for (auto& v: *this)
+				v.set_height(y);
+		}
+		virtual void set_font(void *font)
+		{
+			for (auto& v: *this)
+				v.set_font(font);
+		}
+
+		virtual void render() const
+		{
+			for (auto& v: *this)
+				v.render();
+		}
 };
 
 #endif // AVL_TYPES_H_
