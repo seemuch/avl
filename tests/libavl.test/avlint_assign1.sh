1b72de84a8b67f4d53c2282541f131cde8ff5d39
Qinfan Wu <wuqinfan@gmail.com>
2014-03-20 19:34:06 -0400

    test case updated.

--- tests/libavl.test/avlint_assign1.sh
+++ tests/libavl.test/avlint_assign1.sh
@@ -3,8 +3,16 @@
 SUCCESS="0"
 
 program="./AvlInt_assign1"
-declare -a input=("-100" "-1" "0" "1" "100")
+declare -i n=100
+declare -a input=( "-100" "-1" "0" "1" "100")
 declare -a output=("-100" "-1" "0" "1" "100")
+
+for i in $(seq 1 n)
+do
+	num=$(( ( RANDOM % 20000 ) - 10000 ))
+	input=("${input[@]}" "$num")
+	output=("${output[@]}" "$num")
+done
 
 len=${#input[@]}
 
@@ -17,4 +25,4 @@ do
 	fi
 done
 
-exit $success
+exit $SUCCESS
--- tests/libavl.test/avlint_sub1.sh
+++ tests/libavl.test/avlint_sub1.sh
@@ -2,9 +2,19 @@
 
 SUCCESS="0"
 
-program="./avlint_add1"
-declare -a input=("0 0" "3 1" "1 3" "-1 -2" "-2 -1" "-1 2" "-2 1" "1 -2")
-declare -a output=("0" "2" "-2" "1" "-1" "-3" "-3" "3")
+program="./avlint_sub1"
+declare -i n=100
+declare -a input=( "0 0" "3 1" "1 3" "-1 -2" "-2 -1" "-1 2" "-2 1" "1 -2")
+declare -a output=("0"   "2"   "-2"  "1"     "-1"    "-3"   "-3"   "3")
+
+for i in $(seq 1 n)
+do
+	num1=$(( ( RANDOM % 20000 ) - 10000 ))
+	num2=$(( ( RANDOM % 20000 ) - 10000 ))
+	sub=$(( num1 - num2 ))
+	input=("${input[@]}" "$num1 $num2")
+	output=("${output[@]}" "$sub")
+done
 
 len=${#input[@]}
 
@@ -17,4 +27,4 @@ do
 	fi
 done
 
-exit $success
+exit $SUCCESS
