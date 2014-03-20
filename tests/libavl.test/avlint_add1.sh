9e36068c98aceec56bb116ffc1a6efd570b3ff20
Qinfan Wu <wuqinfan@gmail.com>
2014-03-20 19:34:06 -0400

    test case updated.

--- tests/libavl.test/avlint_add1.sh
+++ tests/libavl.test/avlint_add1.sh
@@ -3,8 +3,18 @@
 SUCCESS="0"
 
 program="./avlint_add1"
-declare -a input=("0 0" "1 2" "-1 -2" "-1 2" "-2 1")
-declare -a output=("0" "3" "-3" "1" "-1")
+declare -i n=100
+declare -a input=( "0 0" "1 2" "-1 -2" "-1 2" "-2 1")
+declare -a output=("0"   "3"   "-3"    "1"    "-1")
+
+for i in $(seq 1 n)
+do
+	num1=$(( ( RANDOM % 20000 ) - 10000 ))
+	num2=$(( ( RANDOM % 20000 ) - 10000 ))
+	sum=$(( num1 + num2 ))
+	input=("${input[@]}" "$num1 $num2")
+	output=("${output[@]}" "$sum")
+done
 
 len=${#input[@]}
 
@@ -17,4 +27,4 @@ do
 	fi
 done
 
-exit $success
+exit $SUCCESS
