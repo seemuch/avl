2bb4ce750a9e4dc59794b8f3b0d525d9b111dcda
Qinfan Wu <wuqinfan@gmail.com>
2014-03-20 20:23:26 -0400

    test scripts updated

--- tests/libavl.test/avlint_sub1.sh
+++ tests/libavl.test/avlint_sub1.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 
-SUCCESS="0"
+SUCCESS=0
 
 program="./avlint_sub1"
 declare -i n=100
 declare -a input=( "0 0" "3 1" "1 3" "-1 -2" "-2 -1" "-1 2" "-2 1" "1 -2")
-declare -a output=("0"   "2"   "-2"  "1"     "-1"    "-3"   "-3"   "3")
 
-for i in $(seq 1 n)
-do
-	num1=$(( ( RANDOM % 20000 ) - 10000 ))
-	num2=$(( ( RANDOM % 20000 ) - 10000 ))
-	sub=$(( num1 - num2 ))
-	input=("${input[@]}" "$num1 $num2")
-	output=("${output[@]}" "$sub")
-done
-
+source genRandom.sh
+genRandom $n 2
+input=("${input[@]}" "${rArray[@]}")
 len=${#input[@]}
 
-for i in $(seq 0 $(( $len - 1 )))
+for i in $(seq 1 $(( len - 1 )))
 do
-	result=`$program ${input[$i]}`
-	if [[ "$result" != "${output[$i]}" ]]
+	num1=`echo ${input[$i]} | awk '{print $1}'`
+	num2=`echo ${input[$i]} | awk '{print $2}'`
+	expected=$(( num1 - num2 ))
+	output=`$program ${input[$i]}`
+
+	if [[ "$output" != "$expected" ]]
 	then
-		SUCCESS="1"
+		SUCCESS=1
 	fi
 done
+
 
 exit $SUCCESS
