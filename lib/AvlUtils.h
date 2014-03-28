61939dd34e32b5c4f79593ca54b93463b6673c95
Qinfan Wu <wuqinfan@gmail.com>
2014-03-28 14:43:15 -0400

    implementation of avlSleep() updated

--- lib/AvlUtils.h
+++ lib/AvlUtils.h
@@ -1,17 +1,29 @@
 #ifndef AVL_UTILS_H_
 #define AVL_UTILS_H_
 
-#include <sys/select.h>
-#include <iostream>
+#include <ctime>
+#include <cerrno>
+
+inline void die(const char *msg)
+{
+	perror(msg);
+	exit(EXIT_FAILURE);
+}
 
 inline void avlSleep(float seconds)
 {
-	timeval tv;
+	timespec req, rem;
 
-	tv.tv_sec = int(seconds);
-	tv.tv_usec = (seconds - int(seconds)) * 1000000;
+	req.tv_sec = int(seconds);
+	req.tv_nsec = (seconds - int(seconds)) * 1000000000;
 
-	select(0, NULL, NULL, NULL, &tv);
+	while (nanosleep(&req, &rem) != 0) {
+		if (errno != EINTR)
+			die("nanosleep() failed.");
+
+		req.tv_sec = rem.tv_sec;
+		req.tv_nsec = rem.tv_nsec;
+	}
 }
 
 #endif // AVL_UTILS_H_
